ae34697247006e860e4c49b2cfa6aed76642ba89
vas-vas777 <vasya1998@list.ru>
2016-10-13 18:52:08 +0300

    Update lab1.Solution2.cpp

--- lab1.Solution2.cpp
+++ lab1.Solution2.cpp
@@ -11,16 +11,16 @@ int main()
 	printf("xA= ");scanf_s("%d", &xA);scanf_s("%c", &A); 
 	printf("xB= ");scanf_s("%d", &xB);scanf_s("%c", &B);    
 	printf("xC= ");scanf_s("%d", &xC);scanf_s("%c", &C); 
-      if (xB-xA>0)  
-	  p=xB-xA;  
-      else p=xA-xB;   
-      if (xC-xA>0)        
-          p2=xC-xA;    
-      else p2=xA-xC;    
-      if (p>p2)       
-          printf("p2=%d %c",p2,C);    
-      else printf ("p=%d %c",p,B);  
-	getch();
+        if (xB-xA>0)  
+	    p=xB-xA;  
+        else p=xA-xB;   
+        if (xC-xA>0)        
+            p2=xC-xA;    
+        else p2=xA-xC;    
+        if (p>p2)       
+            printf("p2=%d %c",p2,C);    
+        else printf ("p=%d %c",p,B);  
+	_getch();
 	return 0;
 }
 
